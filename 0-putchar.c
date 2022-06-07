6ef553bfb73378d55e4f36df0924c11a45f14889
Your Name <you@example.com>
2022-06-07 11:58:03 -0700

    function,nestedloop commit

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,13 +1,10 @@
-#include "main.h"
-
+#include<main.h>
 /**
- * main - Prints _putchar
+ * main - print _putchar
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -18,5 +15,9 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	
+	return 0;
+
 }
+
+
